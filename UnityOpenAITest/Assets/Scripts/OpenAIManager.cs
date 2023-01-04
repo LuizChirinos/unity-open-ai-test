88529ddf91c5f33d8d85660139ec9456c0f6acd2
LuizChirinos <luizmig.mch@gmail.com>
2023-01-04 16:39:02 -0300

    fixed api key

--- UnityOpenAITest/Assets/Scripts/OpenAIManager.cs
+++ UnityOpenAITest/Assets/Scripts/OpenAIManager.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class OpenAIManager : MonoBehaviour
 {
-
     const string API_KEY = "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";
+
     void Start()
     {
         CompleteAsync("A VRGlass é uma empresa fundada em 2011. A VRGlass trabalha com design, desenvolvimento e produção de apps e equipamentos de realidade virtual e aumentada para enterprise e varejo. O que é a VRGlass?");
